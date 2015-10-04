c355d6630a400a9454b3fcd6fd9a84d83509e849
Maxime Fabre <ehtnam6@gmail.com>
2015-10-04 15:29:36 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": [
+        "eslint-config-madewithlove/rules/errors",
+        "eslint-config-madewithlove/rules/best-practices",
+        "eslint-config-madewithlove/rules/node",
+        "eslint-config-madewithlove/rules/style",
+        "eslint-config-madewithlove/rules/variables",
+        "eslint-config-madewithlove/rules/es6",
+        "eslint-config-madewithlove/rules/react",
+        "eslint-config-madewithlove/rules/webpack",
+    ],
+    "rules": {}
+};
