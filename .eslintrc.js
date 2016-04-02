10fa508e449e2185118157c41463d9c976c6ae65
Maxime Fabre <ehtnam6@gmail.com>
2016-04-02 19:16:06 +0200

    Add to rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
         require.resolve("./rules/node"),
         require.resolve("./rules/style"),
         require.resolve("./rules/variables"),
+        require.resolve("./rules/import"),
         require.resolve("./rules/es6"),
         require.resolve("./rules/react"),
         require.resolve("./rules/webpack"),
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = {
         "eslint-config-madewithlove/rules/best-practices",
         "eslint-config-madewithlove/rules/node",
         "eslint-config-madewithlove/rules/style",
+        "eslint-config-madewithlove/rules/import",
         "eslint-config-madewithlove/rules/variables",
         "eslint-config-madewithlove/rules/es6",
         "eslint-config-madewithlove/rules/react",
