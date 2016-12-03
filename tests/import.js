91f72c60f5eaf08742a45e311cf120995c0985bd
Maxime Fabre <ehtnam6@gmail.com>
2016-12-03 10:36:59 +0100

    Update import rules

---
+++ tests/import.js
@@ -0,0 +1,16 @@
+import 'a';
+import 'b';
+import 'c';
+import 'd';
+import 'e';
+import 'f';
+import 'g';
+import 'h';
+import 'i';
+import 'j';
+import 'k';
+import 'l';
+import path from 'path';
+import * as bars from 'foo';
+
+require(path.join(__dirname, bars.foo));
