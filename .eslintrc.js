f7d86daa190cc0783605d5f99f7b86f5d4b35844
Maxime Fabre <ehtnam6@gmail.com>
2018-02-09 14:46:01 +0100

    Make package config always in sync with normal config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,12 @@
-module.exports = {
-    "extends": [
-        "eslint:recommended",
-        require.resolve("./rules/a11y"),
-        require.resolve("./rules/best-practices"),
-        require.resolve("./rules/errors"),
-        require.resolve("./rules/es6"),
-        require.resolve("./rules/filenames"),
-        require.resolve("./rules/flowtype"),
-        require.resolve("./rules/import"),
-        require.resolve("./rules/node"),
-        require.resolve("./rules/prettier"),
-        require.resolve("./rules/promise"),
-        require.resolve("./rules/react"),
-        require.resolve("./rules/security"),
-        require.resolve("./rules/style"),
-        require.resolve("./rules/variables"),
-        require.resolve("./rules/webpack"),
-    ],
+const configuration = require('./index');
+
+module.exports = Object.assign(configuration, {
+    extends: configuration.extends.map(extended => {
+        return extended.includes('madewithlove') ? require.resolve(extended.replace('eslint-config-madewithlove', '.')) : extended;
+    }),
     "rules": {
         "import/no-unresolved": 0,
-        "import/no-extraneous-dependencies": 0
+        "import/no-extraneous-dependencies": 0,
     },
-};
+});
 
