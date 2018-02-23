553bc076761946e933910456e21a89d51613d317
Maxime Fabre <ehtnam6@gmail.com>
2018-02-23 15:18:28 +0100

    Keep prettier rules in sync

--- rules/prettier.js
+++ rules/prettier.js
@@ -1,4 +1,9 @@
+const { prettier } = require('../package');
+
 module.exports = {
     plugins: ['prettier'],
     extends: ['plugin:prettier/recommended'],
+    rules: {
+        'prettier/prettier': ['warn', prettier],
+    },
 };
